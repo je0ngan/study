26caaf47a64ec7fd8486249330d34c294687dcf4
je0ngan <jeo0ngan@gmail.com>
2024-06-04 15:06:53 +0900

    docs: 자바스크립트 스터디

---
+++ 01/js/dataType.js
@@ -0,0 +1,5 @@
+const x = 5 > 3;
+const y = 5 < 3;
+
+console.log(x); // true
+console.log(y); // false
---
+++ 01/js/script.js
@@ -0,0 +1,5 @@
+console.log("안녕");
+console.log("Hello");
+console.log("Hi");
+
+document.getElementById("show").innerHTML = "안녕하세요.";
